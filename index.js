10e2d7e2313d30b6f3d5bf5f9afc56afa9a2e597
Matthew <phillips.matthew829@gmail.com>
2021-11-03 10:47:46 -0500

    commit tree 924ef1181ac6b5bda3f51178762d84b9819136ea

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+    return Math.abs(42 - num);
+}
+
+function distanceFromHqInFeet(num){
+    return distanceFromHqInBlocks(num) * 264;    
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distance = Math.abs(start - destination) * 264;
+    return distance;
+}
+
+function calculatesFarePrice(start, destination){
+    distanceFromHqInFeet(start, destination);
+
+    let distance = Math.abs(start - destination) * 264;
+
+    if(distance <= 400){
+        return 0; //$0
+    }
+    else if (distance >= 401 && distance <= 2000){
+        return (distance - 400) * 0.02; //Pay 2 cents per foot, excluding the first 400 feet
+    }
+    else if(distance >= 2001 && distance <= 2500){
+        return 25; //$25
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
